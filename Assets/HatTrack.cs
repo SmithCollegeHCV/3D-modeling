3b41c981f635c0559eef202b20bd149fdc0c0258
Evan Reilly <ereilly1@uvm.edu>
2017-05-30 11:05:40 -0400

    Initial Commit, working floating hat, Unity environment set up

---
+++ Assets/HatTrack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+public class HatTrack : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    GameObject head; 
+	void Update () {
+        GameObject Head = GameObject.Find("Head");
+
+        if (Head)
+        {
+            gameObject.transform.position = Head.transform.position +Vector3.up*2;
+        }
+	}
+}
